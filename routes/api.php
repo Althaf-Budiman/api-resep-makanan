1076f333136ffc33a60e8d6bf0ec05d5ef11618f
Althaf-Budiman <budimanaltaf687@gmail.com>
2023-03-22 13:40:23 +0700

    get/check data user

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,8 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/recipes', [RecipeController::class, 'index']);
     Route::get('/recipes/{id}', [RecipeController::class, 'show']);
     Route::post('/logout', [AuthenticationController::class, 'logout']);
+
+    Route::get('/me', [AuthenticationController::class, 'me']);
 });
 
 Route::post('/login', [AuthenticationController::class, 'login']);
