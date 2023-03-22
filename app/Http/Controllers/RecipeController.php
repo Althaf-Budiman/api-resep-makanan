a4d2091fcdc981f03f749882db745bc6c1866d86
Althaf-Budiman <budimanaltaf687@gmail.com>
2023-03-22 10:57:56 +0700

    return api with resource

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\RecipeResource;
 use App\Models\Recipe;
 use Illuminate\Http\Request;
 
@@ -10,6 +11,7 @@ class RecipeController extends Controller
     public function index()
     {
         $recipes = Recipe::all();
-        return response()->json($recipes);
+        // return response()->json($recipes);
+        return RecipeResource::collection($recipes);
     }
 }
