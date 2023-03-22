bf2c59cf22cf05fcd02655abbd910f99f6b92c0e
Althaf-Budiman <budimanaltaf687@gmail.com>
2023-03-22 13:32:02 +0700

    logout feature with token

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,10 @@ use App\Http\Controllers\RecipeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth.sanctum'])->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/recipes', [RecipeController::class, 'index']);
     Route::get('/recipes/{id}', [RecipeController::class, 'show']);
+    Route::post('/logout', [AuthenticationController::class, 'logout']);
 });
 
 Route::post('/login', [AuthenticationController::class, 'login']);
